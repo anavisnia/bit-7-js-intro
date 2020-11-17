badb67330a814d86a18d392fd373bd617c5f1b01
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-11-17 16:51:47 +0200

    kas kelintos reides istraukimas is teksto

---
+++ js/selectLetters.js
@@ -0,0 +1,73 @@
+function pickLetters(text, step) {
+    // input validation
+    if (typeof text !== 'string') {
+        return 'ERROR: pirmoji reiksme ne teksto tipo.'
+    }
+    if (text === '') {
+        return 'ERROR: tekstas negali buti tuscias.'
+    }
+    if (typeof step !== 'number') {
+        return 'ERROR: antroji reiksme ne skaiciaus tipo.'
+    }
+
+    const size = text.length;
+
+    if (step > size) {
+        return 'ERROR: zinfsnis yra per didelis lyginant su teksto ilgiu'
+    }
+    if (step === 0) {
+        return 'ERROR: zingsnis negali buti nulinis'
+    }
+    if (step % 1 !== 0) {
+        return 'ERROR: zingsnis turi buti sveikasis skaicius.';
+    }
+
+    // logic
+    // reikia sukunstruuoti atsakima
+    let rez = '';
+
+    if (step > 0) {
+        const firstLetterPosition = step - 1;
+        for (let i = firstLetterPosition; i < size; i+= step) {
+            rez += text[i];
+        };
+    } else {
+        const firstLetterPosition = size + step;
+        for (let i = firstLetterPosition; i >= 0; i+= step) {
+            rez += text[i];
+        };
+    }
+
+
+    
+    // post logic validation
+
+
+    // return
+    return rez;
+}
+
+console.log(pickLetters(1561,2));
+console.log(pickLetters('1561','2'));
+console.log(pickLetters('abc',4));
+console.log(pickLetters('abc',0));
+console.log(pickLetters('abijkl',1.4), '->', 'cfil');
+console.log(pickLetters('abcdefghijkl',1.2), '->', 'cfil');
+console.log('-------------------------------------');
+console.log(pickLetters('abc',3), '->', 'c');
+console.log(pickLetters('abcdefg',2), '->', 'bdf');
+console.log(pickLetters('abcdefghijkl',3), '->', 'cfil');
+console.log('-------------------------------------');
+console.log(pickLetters('abc',-3), '->', 'a');
+console.log(pickLetters('abcdefg',-2), '->', 'fdb');
+console.log(pickLetters('abcdefghijkl',-3), '->', 'jgda');
+console.log('-------------------------------------');
+
+console.log(pickLetters('',-3));
+
+/*
+step: 3
+  _  _  _  _
+abcdefghijkl -? cfil
+---------
+*/
